6b7c7ea41808cf2da96514f0c47d16dd98155f21
stevensotelo <stevenbetancurt@hotmail.com>
2018-01-26 16:11:02 -0500

    improved the function calculate_grs in insitu package. It now has trycatch section to manage the errors

--- src/2_gap_analysis/insitu/grs/grs.R
+++ src/2_gap_analysis/insitu/grs/grs.R
@@ -1,6 +1,6 @@
 ##########################################  Start Install Packages  ###############################################
 
-# install.packages(c("raster","sp","rgdal","rgeos","sf","shapefiles"))
+# install.packages(c("raster","sp","rgdal","rgeos","sf","shapefiles", "snowfall"))
 
 ##########################################   End Install Packages  ###############################################
 
@@ -11,6 +11,7 @@
 require(raster)
 require(rgdal)
 require(sf)
+library(snowfall)
 
 ##########################################   End Requirements  ###############################################
 
@@ -45,68 +46,93 @@ species.list = list.dirs(species.dir,full.names = FALSE, recursive = FALSE)
 # @return (void): This function does not return nothing
 calculate_grs = function(specie){
   
-  print(paste0("Start ",specie))
+  message = "Ok"
+  status = TRUE
   
-  # Set the global
-  specie.dir = paste0(species.dir, specie, "/")
-  specie = gsub(".csv", "", specie)
-  
-  # Load the specie raster (specie distribution)
-  specie.distribution = raster(paste0(specie.dir, "concenso_mss.tif"))
-  # Remove the zeros (0) from raster
-  specie.distribution[which(specie.distribution[]==0)]<-NA
-  
-  print("Loaded the specie distribution file (raster)")
-  
-  # Intersect betwenn specie distribution and protected areas
-  overlay = pa.raster * specie.distribution
-  
-  print("Intersected the specie distribution and global protected areas")
-  
-  # Get pixels with data from intersect
-  a = which(!is.na(overlay[]))
-  # Get pixels with data from specie distribution
-  b = which(!is.na(specie.distribution[]))
-  
-  # Calculating the area in kilometer for each pixel
-  area = res(specie.distribution)[1] * res(specie.distribution)[2]
-  gra = 111.11*111.11
-  res = area * gra 
-  
-  # Calculate areas for the specie distribution and intersect
-  overlay.area <- length(a) * res
-  specie.area <- length(b) * res
-  
-  # Calculate proportion area
-  proportion = (overlay.area / specie.area) * 100
-  
-  print("Calculated the areas and proportions")
-  
-  # Join the results
-  df <- data.frame(specie_distribution_a = specie.area, species_protected_area_a = overlay.area, units_a = c("km2"), proportion = proportion, units_proportion = c("percentage"))
-  
-  # Create output dirs
-  if(!dir.exists(paste0(specie.dir,"gap_analysis"))){
-    dir.create(paste0(specie.dir,"gap_analysis"))
-  }
-  if(!dir.exists(paste0(specie.dir,"gap_analysis/insitu"))){
-    dir.create(paste0(specie.dir,"gap_analysis/insitu"))
-  }
-  # Save the results
-  species.output = paste0(specie.dir,"gap_analysis/insitu/grs")
-  dir.create(species.output)
-  write.csv(df, paste0(species.output,"/result.csv"), row.names = FALSE, quote = FALSE)
-  writeRaster(overlay, paste0(species.output,"/intersect.tif"))
-  
-  # Remove temp files
-  removeTmpFiles(h=0)
-  
-  print(paste0("End ",specie))
+  tryCatch({
+    print(paste0("Start ",specie))
+    
+    # Set the global
+    specie.dir = paste0(species.dir, specie, "/")
+    specie = gsub(".csv", "", specie)
+    
+    # Load the specie raster (specie distribution)
+    specie.distribution = raster(paste0(specie.dir, "concenso_mss.tif"))
+    # Remove the zeros (0) from raster
+    specie.distribution[which(specie.distribution[]==0)]<-NA
+    
+    print("Loaded the specie distribution file (raster)")
+    
+    # Intersect betwenn specie distribution and protected areas
+    origin(pa.raster) <- origin(specie.distribution)
+    overlay = pa.raster * specie.distribution
+    
+    print("Intersected the specie distribution and global protected areas")
+    
+    # Get pixels with data from intersect
+    a = which(!is.na(overlay[]))
+    # Get pixels with data from specie distribution
+    b = which(!is.na(specie.distribution[]))
+    
+    # Calculating the area in kilometer for each pixel
+    area = res(specie.distribution)[1] * res(specie.distribution)[2]
+    gra = 111.11*111.11
+    res = area * gra 
+    
+    # Calculate areas for the specie distribution and intersect
+    overlay.area <- length(a) * res
+    specie.area <- length(b) * res
+    
+    # Calculate proportion area
+    proportion = (overlay.area / specie.area) * 100
+    
+    print("Calculated the areas and proportions")
+    
+    # Join the results
+    df <- data.frame(specie_distribution_a = specie.area, species_protected_area_a = overlay.area, units_a = c("km2"), proportion = proportion, units_proportion = c("percentage"))
+    
+    # Create output dirs
+    if(!dir.exists(paste0(specie.dir,"gap_analysis"))){
+      dir.create(paste0(specie.dir,"gap_analysis"))
+    }
+    if(!dir.exists(paste0(specie.dir,"gap_analysis/insitu"))){
+      dir.create(paste0(specie.dir,"gap_analysis/insitu"))
+    }
+    # Save the results
+    species.output = paste0(specie.dir,"gap_analysis/insitu/grs")
+    dir.create(species.output)
+    write.csv(df, paste0(species.output,"/result.csv"), row.names = FALSE, quote = FALSE)
+    writeRaster(overlay, paste0(species.output,"/intersect.tif"),overwrite=T )
+    return (data.frame(specie = specie, status = status, message = message))
+    
+  }, 
+  error = function(e) {
+    
+    message = e
+    status = FALSE
+    return (data.frame(specie = specie, status = status, message = message[[1]]))
+  }, finally = {
+    
+    # Remove temp files
+    removeTmpFiles(h=0)
+    
+    print(paste0("End ",specie))
+  })
 }
 ##########################################    End Functions    ###############################################
 
 
 ##########################################   Start Process    ###############################################
 
-# specie = species.list[10]
-# lapply(species.list[1],calculate_grs)
+# sfInit(parallel = T, cpus = 20)
+# sfLibrary(raster)
+# sfLibrary(rgdal)
+# sfLibrary(sf)
+# sfExportAll()
+# sfExport("calculate_grs")
+# 
+# sfLapply(species.list,calculate_grs)
+
+# specie = species.list[7]
+# lapply(species.list[7],calculate_grs)
+result = lapply(species.list,calculate_grs)
