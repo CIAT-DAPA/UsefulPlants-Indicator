68375f874fa4ee2d2edee3c99cf5706af33750d4
stevensotelo <stevenbetancurt@hotmail.com>
2018-01-30 08:50:27 -0500

    started with script ers insitu

---
+++ src/2_gap_analysis/insitu/ers.R
@@ -0,0 +1,158 @@
+##########################################  Start Install Packages  ###############################################
+
+# install.packages(c("raster","sp","rgdal","rgeos","sf","shapefiles", "snowfall"))
+
+##########################################   End Install Packages  ###############################################
+
+
+##########################################  Start Requirements  ###############################################
+
+# Load the libraries
+require(raster)
+require(rgdal)
+require(sf)
+library(snowfall)
+library(plyr)
+
+##########################################   End Requirements  ###############################################
+
+
+##########################################  Start Set Parameters  ###############################################
+
+# Global configuration
+rasterOptions(tmpdir = "D:/TEMP/hsotelo")
+setwd("//dapadfs/Projects_cluster_9/aichi/")
+
+# Set the path of the file with global protected areas
+pa.path = "WDPA/areas_protected_geographic.tif"
+# Load the raster file with global protected areas
+pa.raster = raster(pa.path)
+# Remove the zeros (0) from raster
+pa.raster[which(pa.raster[] == 0)] <- NA
+# Load the species list to execute process
+species.dir = "ENMeval_4/outputs/"
+species.list = list.dirs(species.dir,full.names = FALSE, recursive = FALSE)
+# Set the path of the file with global ecosystem
+eco.path = "ecosystem/wwf_eco_terr_geo.tif"
+eco.raster = raster(eco.path)
+
+
+##########################################   End Set Parameters  ###############################################
+
+
+##########################################   Start Functions    ###############################################
+
+# This function calculate the ERS by every specie.
+# It searches the specie, then load the specie distribution from raster file. 
+# With the specie distribution intersectes with the protected areas raster and calculate
+# the area from the specie distribution, overlay and the proportion between both.
+# It creates two files with the result (result.csv, intersect.tif)
+# @param (string) specie: Code of the specie
+# @return (data.frame): This function return a dataframe with the results about the process. 
+#                       It has three columns, the first has the specie code; the second has a status
+#                       of process, if value is "TRUE" the process finished good, if the result is "FALSE"
+#                       the process had a error; the third column has a description about process
+calculate_ers = function(specie){
+  
+  message = "Ok"
+  status = TRUE
+  
+  tryCatch({
+    print(paste0("Start ",specie))
+    
+    # Set the global
+    specie.dir = paste0(species.dir, specie, "/")
+    specie = gsub(".csv", "", specie)
+    
+    # Load the specie raster (specie distribution)
+    specie.distribution = raster(paste0(specie.dir, "concenso_mss.tif"))
+    # Remove the zeros (0) from raster
+    specie.distribution[which(specie.distribution[]==0)]<-NA
+    
+    print("Loaded the specie distribution file (raster)")
+    
+    # Intersect between specie distribution and ecosystem
+    origin(eco.raster) <- origin(specie.distribution)
+    overlay.eco = eco.raster * specie.distribution
+    
+    print("Intersected the specie distribution and ecosystem")
+    
+    # Intersect between overlay eco specie  and protected areas
+    origin(pa.raster) <- origin(overlay.eco)
+    overlay.pa = pa.raster * overlay.eco
+    
+    print("Intersected the overlapping (specie distribution and ecosystems) and global protected areas")
+    
+    # Get pixels with data from intersect
+    a = which(!is.na(overlay[]))
+    # Get pixels with data from specie distribution
+    b = which(!is.na(specie.distribution[]))
+    
+    # Calculating the area in kilometer for each pixel
+    area = res(specie.distribution)[1] * res(specie.distribution)[2]
+    gra = 111.11*111.11
+    res = area * gra 
+    
+    # Calculate areas for the specie distribution and intersect
+    overlay.area <- length(a) * res
+    specie.area <- length(b) * res
+    
+    # Calculate proportion area
+    proportion = (overlay.area / specie.area) * 100
+    
+    print("Calculated the areas and proportions")
+    
+    # Join the results
+    df <- data.frame(specie_distribution_a = specie.area, species_protected_area_a = overlay.area, units_a = c("km2"), proportion = proportion, units_proportion = c("percentage"))
+    
+    # Create output dirs
+    if(!dir.exists(paste0(specie.dir,"gap_analysis"))){
+      dir.create(paste0(specie.dir,"gap_analysis"))
+    }
+    if(!dir.exists(paste0(specie.dir,"gap_analysis/insitu"))){
+      dir.create(paste0(specie.dir,"gap_analysis/insitu"))
+    }
+    # Save the results
+    species.output = paste0(specie.dir,"gap_analysis/insitu/ers")
+    dir.create(species.output)
+    write.csv(df, paste0(species.output,"/result.csv"), row.names = FALSE, quote = FALSE)
+    writeRaster(overlay, paste0(species.output,"/intersect.tif"),overwrite=T )
+    return (data.frame(specie = specie, status = status, message = message))
+    
+  }, 
+  error = function(e) {
+    
+    message = e
+    status = FALSE
+    return (data.frame(specie = specie, status = status, message = message[[1]]))
+  }, finally = {
+    
+    # Remove temp files
+    removeTmpFiles(h=0)
+    
+    print(paste0("End ",specie))
+  })
+}
+##########################################    End Functions    ###############################################
+
+
+##########################################   Start Process    ###############################################
+
+# Set a configuration to parallel the execution of function
+sfInit(parallel = T, cpus = 20)
+sfLibrary(raster)
+sfLibrary(rgdal)
+sfLibrary(sf)
+sfExportAll()
+sfExport("calculate_grs")
+
+# Run function in parallel for all species
+result = sfLapply(species.list,calculate_grs)
+
+# specie = species.list[7]
+# lapply(species.list[7],calculate_grs)
+# result = lapply(species.list,calculate_grs)
+
+# Get the results for all species
+df <- ldply(result, data.frame)
+write.csv(df, paste0("C:/Users/HSOTELO/Desktop/summary.csv"), row.names = FALSE, quote = FALSE)
