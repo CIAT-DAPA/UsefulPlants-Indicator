ca20856b01ef89d85154e130991db30da82bb36b
haachicanoy <harold22010@gmail.com>
2018-01-31 15:27:40 -0500

    Modeling script updated

--- src/1_modeling/1_1_maxent/modeling_approach.R
+++ src/1_modeling/1_1_maxent/modeling_approach.R
@@ -7,147 +7,148 @@ library(tidyverse)
 library(dismo)
 library(velox)
 
-# Occurrence points
-occ_data <- read_csv(file = "//dapadfs/Projects_cluster_9/aichi/ENMeval_4/ocurrences/2979057.csv")
-occ_data <- as.data.frame(occ_data)
+# Obtain optimum parameters: Using ENMevaluate
+source("//dapadfs/Projects_cluster_9/aichi/scripts/CreateMXArgs.R")
 
-# Determine background points
-msk <- raster("//dapadfs/Workspace_cluster_9/Aichi13/parameters/world_mask/raster/mask.tif")
-msk_pts <- raster::rasterToPoints(msk)
-msk_pts <- as.data.frame(msk_pts)
-msk_pts <- msk_pts[msk_pts$y > -60,]
-msk_pts$mask <- NULL
-names(msk_pts) <- c("lon", "lat")
-msk_pts$cellID <- cellFromXY(object = msk, xy = msk_pts[,c("lon", "lat")])
-occ_cellID <- cellFromXY(object = msk, xy = occ_data[,c("lon", "lat")])
-msk_pts <- msk_pts[setdiff(msk_pts$cellID, occ_cellID),]; rm(occ_cellID)
-rownames(msk_pts) <- 1:nrow(msk_pts)
+# Load calibration results
+load("//dapadfs/Projects_cluster_9/aichi/ENMeval_4/outputs/2979057.csv/2979057.csv.RData")
 
-if(nrow(occ_data) >= 50){
-  set.seed(1234)
-  smpl <- base::sample(x = 1:nrow(msk_pts), size = nrow(occ_data)*10, replace = F)
-  bck_data <- msk_pts[smpl,]; rm(smpl)
-} else {
-  set.seed(1234)
-  smpl <- base::sample(x = 1:nrow(msk_pts), size = nrow(occ_data)*100, replace = F)
-  bck_data <- msk_pts[smpl,]; rm(smpl)
+# Define output folder
+crossValDir <- "//dapadfs/Projects_cluster_9/aichi/ENMeval_4/outputs/2979057.csv/modeling_results"
+
+# Fitting final model
+tryCatch(expr = {
+  fit <- dismo::maxent(x = rst_fls, # Climate
+                       p = optPars@occ.pts[,c("LON","LAT")], # Occurrences
+                       a = optPars@bg.pts[,c("LON","LAT")], # Pseudo-absences
+                       removeDuplicates = T,
+                       # args = c("nowarnings","replicates=5","linear=true","quadratic=true","product=true","threshold=true","hinge=true","pictures=false","plots=false"),
+                       args = c("nowarnings","replicates=5","pictures=false","plots=false",CreateMXArgs(optPars)),
+                       path = crossValDir)
+},
+error = function(e){
+  cat("Modeling process failed:", unique(occ_data$species), "\n")
+  return("Done\n")
+})
+
+# Native area to project
+load("//dapadfs/Projects_cluster_9/aichi/ENMeval_4/outputs/2979057.csv/narea/crop_narea.RDS")
+
+# Extract climate data
+system.time(expr = {
+  pnts <- rasterToPoints(x = biolayers_cropc)
+  pnts <- as.data.frame(pnts)
+  pnts$cellID <- cellFromXY(object = biolayers_cropc[[1]], xy = pnts[,1:2])
+})
+# system.time(expr = {
+#   tst_vx <- velox::velox(biolayers_cropc)
+#   pnts2 <- tst_vx$extract_points(sp = sp::Spatial(tst_vx$getCoordinates()))
+#   pnts2 <- as.data.frame(pnts2)
+# })
+
+source("do_projections.R")
+
+setwd(crossValDir)
+
+# Do projections
+system.time(expr = {
+  pred <- raster::stack(lapply(1:5, function(x) make.projections(x, pnts = pnts, tmpl_raster = biolayers_cropc[[1]])))
+})
+system.time(expr = {pred2 <- predict(fit, biolayers_cropc)})
+
+par(mfrow = c(2,3))
+for(i in 1:5){
+  hist(pred[[i]][!is.na(pred[[i]][])] - pred2[[i]][!is.na(pred2[[i]][])])
 }
-bck_data$cellID <- NULL
-bck_data$species <- unique(occ_data$species)
-bck_data <- bck_data[complete.cases(bck_data),] # Check this
 
-# Load raster stack of climate variables
-rst_dir <- "//dapadfs/Projects_cluster_9/aichi/biolayer_2.5"
-rst_fls <- list.files(path = rst_dir, full.names = T)
-rst_fls <- rst_fls[grep(pattern = "*.tif$", x = rst_fls)]
-rst_fls <- raster::stack(rst_fls)
-rst_fls_vx <- velox::velox(rst_fls)
+j.size <- "-mx8000m"
+maxentApp <- "C:/Users/HAACHICANOY/Documents/R/win-library/3.4/dismo/java/maxent.jar"
+projLayers <- "C:/Users/HAACHICANOY/Downloads/Climate"
 
-# Calibration process
-library(devtools)
-if(!require(enmSdm)){
-  install_github('adamlilith/omnibus')
-  install_github('adamlilith/enmSdm')
-  library(omnibus)
-  library(enmSdm)
-} else {
-  library(omnibus)
-  library(enmSdm)
+# outDir <- "C:/Users/HAACHICANOY/Downloads/Climate"
+# for(i in 1:nlayers(biolayers_cropc)){
+#   writeRaster(x = biolayers_cropc[[i]], filename = paste0(outDir, "/", names(biolayers_cropc)[i], ".asc"))
+# }
+
+for(i in 1:5){
+  lambdaFile <- paste0("C:/Users/HAACHICANOY/Downloads/Model2/species_", i-1, ".lambdas")
+  outGrid <- paste0("C:/Users/HAACHICANOY/Desktop/fold", i-1, ".asc")
+  system(paste("java", j.size, "-cp", maxentApp, "density.Project", lambdaFile, projLayers, outGrid, "nowarnings", "fadebyclamping", "-r", "-a", "-z"), wait=TRUE)
 }
-transformInputData <- function(occ = occ_data, bck = bck_data, clim = rst_fls_vx){
-  occ$y <- 1
-  bck$y <- 0
-  all_data <- rbind(occ, bck)
-  all_data <- cbind(all_data, rst_fls_vx$extract_points(sp = sp::SpatialPoints(all_data[,c("lon", "lat")])))
-  names(all_data)[5:ncol(all_data)] <- names(rst_fls)
-  return(all_data)
-}
-all_data <- transformInputData(occ = occ_data, bck = bck_data, clim = rst_fls_vx)
 
-maxent_calibration <- trainMaxEnt(data = all_data[,4:ncol(all_data)], regMult = seq(0, 6, 0.5), out = c('model', 'tuning'), verbose = TRUE)
+javaFold_0 <- raster::raster("C:/Users/HAACHICANOY/Desktop/fold0.asc")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+lambdaFile <- paste0("C:/Users/HAACHICANOY/Downloads/Model2/species_0.lambdas")
+outGrid <- paste0("C:/Users/HAACHICANOY/Desktop/fold0.asc")
+system(paste("java", j.size, "-cp", maxentApp, "density.Project", lambdaFile, projLayers, outGrid, "nowarnings", "fadebyclamping", "-r", "-a", "-z"), wait=TRUE)
 
 # ================================================================================================================================= #
-# 
-# # Load libraries
-# library(tidyverse)
-# library(dismo)
-# 
-# # Load calibration results
-# load("//dapadfs/Projects_cluster_9/aichi/ENMeval_4/outputs/2979057.csv/2979057.csv.RData")
+# Another calibration approach
+# ================================================================================================================================= #
 # 
 # # Occurrence points
-# optPars@occ.pts
 # occ_data <- read_csv(file = "//dapadfs/Projects_cluster_9/aichi/ENMeval_4/ocurrences/2979057.csv")
+# occ_data <- as.data.frame(occ_data)
 # 
-# # Background points
-# optPars@bg.pts
+# # Determine background points
+# msk <- raster("//dapadfs/Workspace_cluster_9/Aichi13/parameters/world_mask/raster/mask.tif")
+# msk_pts <- raster::rasterToPoints(msk)
+# msk_pts <- as.data.frame(msk_pts)
+# msk_pts <- msk_pts[msk_pts$y > -60,]
+# msk_pts$mask <- NULL
+# names(msk_pts) <- c("lon", "lat")
+# msk_pts$cellID <- cellFromXY(object = msk, xy = msk_pts[,c("lon", "lat")])
+# occ_cellID <- cellFromXY(object = msk, xy = occ_data[,c("lon", "lat")])
+# msk_pts <- msk_pts[setdiff(msk_pts$cellID, occ_cellID),]; rm(occ_cellID)
+# rownames(msk_pts) <- 1:nrow(msk_pts)
 # 
-# # Obtain optimum parameters
-# source("//dapadfs/Projects_cluster_9/aichi/scripts/CreateMXArgs.R")
+# if(nrow(occ_data) >= 50){
+#   set.seed(1234)
+#   smpl <- base::sample(x = 1:nrow(msk_pts), size = nrow(occ_data)*10, replace = F)
+#   bck_data <- msk_pts[smpl,]; rm(smpl)
+# } else {
+#   set.seed(1234)
+#   smpl <- base::sample(x = 1:nrow(msk_pts), size = nrow(occ_data)*100, replace = F)
+#   bck_data <- msk_pts[smpl,]; rm(smpl)
+# }
+# bck_data$cellID <- NULL
+# bck_data$species <- unique(occ_data$species)
+# bck_data <- bck_data[complete.cases(bck_data),] # Check this
 # 
 # # Load raster stack of climate variables
-# rst_dir <- "//dapadfs/Projects_cluster_9/aichi/biolayer_2.5"
-# rst_fls <- list.files(path = rst_dir, full.names = T)
-# rst_fls <- rst_fls[grep(pattern = "*.tif$", x = rst_fls)]
-# rst_fls <- raster::stack(rst_fls)
+# if(!file.exists("//dapadfs/Workspace_cluster_9/Aichi13/gap_analysis/climate_vx.RDS")){
+#   rst_dir <- "//dapadfs/Projects_cluster_9/aichi/biolayer_2.5"
+#   rst_fls <- list.files(path = rst_dir, full.names = T)
+#   rst_fls <- rst_fls[grep(pattern = "*.tif$", x = rst_fls)]
+#   rst_fls <- raster::stack(rst_fls)
+#   rst_fls_vx <- velox::velox(rst_fls)
+#   saveRDS(object = rst_fls_vx, file = "//dapadfs/Workspace_cluster_9/Aichi13/gap_analysis/climate_vx.RDS")
+# } else {
+#   rst_fls_vx <- readRDS("//dapadfs/Workspace_cluster_9/Aichi13/gap_analysis/climate_vx.RDS")
+# }
 # 
-# # Define output folder
-# crossValDir <- "C:/Users/HAACHICANOY/Downloads/Model2"
+# # Calibration process
+# transformInputData <- function(occ = occ_data, bck = bck_data, clim = rst_fls_vx){
+#   occ$y <- 1
+#   bck$y <- 0
+#   all_data <- rbind(occ, bck)
+#   all_data <- cbind(all_data, rst_fls_vx$extract_points(sp = sp::SpatialPoints(all_data[,c("lon", "lat")])))
+#   names(all_data)[5:ncol(all_data)] <- names(rst_fls)
+#   return(all_data)
+# }
+# all_data <- transformInputData(occ = occ_data, bck = bck_data, clim = rst_fls_vx)
+# library(devtools)
+# if(!require(enmSdm)){
+#   install_github('adamlilith/omnibus')
+#   install_github('adamlilith/enmSdm')
+#   library(omnibus)
+#   library(enmSdm)
+# } else {
+#   library(omnibus)
+#   library(enmSdm)
+# }
 # 
-# # Run maxent
-# tryCatch(expr={
-#   fit <- dismo::maxent(x = rst_fls, # Climate
-#                        p = optPars@occ.pts[,c("LON","LAT")], # Occurrences
-#                        a = optPars@bg.pts[,c("LON","LAT")], # Pseudo-absences
-#                        removeDuplicates = T,
-#                        # args = c("nowarnings","replicates=5","linear=true","quadratic=true","product=true","threshold=true","hinge=true","pictures=false","plots=false"),
-#                        args = c("nowarnings","replicates=5","pictures=false","plots=false",CreateMXArgs(optPars)),
-#                        path = crossValDir)
-# },
-# error=function(e){
-#   cat("Modeling process failed:",unique(occ_data$species),"\n")
-#   return("Done\n")
-# })
-# 
-# setwd(crossValDir)
-# 
-# files <- list.files(path = crossValDir, full.names = T)
-# files_filtered <- files[grep(pattern = "*.lambdas$", x = files)]
-# file.remove(setdiff(files, files_filtered))
-# 
-# # Obtain climate data information from raster
-# 
-# pnts <- vx$extract_points(sp = sp::Spatial(temp.dt$getCoordinates()))
-# 
-# temp.dt <- velox::velox(rst_fls)
-# 
-# system.time(expr = {pred1 <- predict(fit, rst_fls)})
-# system.time(expr = {pred2 <- raster::stack(lapply(1:5, make.projections))})
-# 
-# 
-# aic.opt <- optPars@models[[which(optPars@results$delta.AICc==0)]]
-# aic.opt@results
-
-
-
+# library(microbenchmark)
+# # system.time(expr = {maxent_calibration <- enmSdm::trainMaxEnt(data = all_data[,4:ncol(all_data)], regMult = seq(0.5, 4, 0.5), out = c('tuning'), verbose = TRUE, jackknife = F)})
+# microbenchmark("Default" = {maxent_calibration <- enmSdm::trainMaxEnt(data = all_data[,4:ncol(all_data)], regMult = seq(0.5, 4, 0.5), out = c('tuning', 'model'), verbose = TRUE)},
+#                "PO some things" = {maxent_calibration <- enmSdm::trainMaxEnt(data = all_data[,4:ncol(all_data)], regMult = seq(0.5, 4, 0.5), out = c('tuning'), verbose = TRUE, jackknife = F)})
