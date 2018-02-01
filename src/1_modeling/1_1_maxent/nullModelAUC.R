d9d3bfc4863044717a38eaf4b89a0d32f5ecd256
Chrystian <chrystiam.sossa@gmail.com>
2018-02-01 16:12:40 -0500

    Null Model General function used to calculate cAUC (Hijmans,2012) http://onlinelibrary.wiley.com/doi/10.1890/11-0826.1/abstract

---
+++ src/1_modeling/1_1_maxent/nullModelAUC.R
@@ -0,0 +1,41 @@
+require(dismo);require(raster)
+
+#run_version<-"v1"
+#gap_dir<-"//dapadfs/Workspace_cluster_9/Aichi13/gap_analysis"
+null_model_AUC<-function(sp){
+  
+  ####LOADING CALIBRATION OCCURRENCES AND NATIVE AREA MASK
+  
+  narea_mask<-raster(paste0(gap_dir,"/",sp,"/",run_version,"/","bioclim/narea_mask.tif"))
+  load(paste0(gap_dir, "/", sp, "/", run_version, "/modeling/maxent/", sp, ".csv.RData"))
+  
+ # narea_mask<-raster("//dapadfs/Workspace_cluster_9/Aichi13/gap_analysis/2653304/v1/bioclim/narea_mask.tif")
+
+  ####ADJUSTING AN UNIQUE SET.SEED TO GET THE SAME KFOLD SPLIT
+  
+  set.seed(1235)
+  occs<-optPars@occ.pts
+  training<-occs[which(kfold(occs, k=3)!=3),]
+  rownames(training) <- 1:nrow(training)
+  colnames(training)<-c("lon","lat")
+  
+  set.seed(1235)
+  testing  <- occs[which(kfold(occs, k=3)==3),]
+  rownames(testing) <- 1:nrow(testing)
+  colnames(testing)<-c("lon","lat")
+  
+  ####PERFORMING NULL MODEL USING GEODIST FUNCTION AND 10000 PSEUDOABSENCES
+  Null_model <- geoDist(training, lonlat=T)
+  
+  # Step 4: Evaluate model
+  pabs <- randomPoints(mask=narea_mask, n=10000)
+  pabs <- as.data.frame(pabs)
+  names(pabs) <- c("lon", "lat")
+  eval <- dismo::evaluate(p=testing, a=pabs, model=Null_model)
+  AUC <- eval@auc
+  
+  
+  return(AUC)
+}
+
+#x<-null_model_AUC(2653304)
