24eeb920c243ffff788d1a7af8a3d1f3f2543b37
Maria Victoria <diaz.maria@correounivalle.edu.co>
2018-02-01 13:54:29 -0500

    function with config

--- src/jonatan/nat_area_mask.R
+++ src/jonatan/nat_area_mask.R
@@ -1,32 +1,43 @@
 require(shapefiles);require(raster);library(rgeos);require(rgdal); require(parallel)
 
-outfol <- "//dapadfs/Projects_cluster_9/aichi/ENMeval_4/outputs/"
+#make source
 
-setwd("//dapadfs/Projects_cluster_9/aichi/biolayer_2.5")
+#load config function
+config(dirs=T, premodeling=T)
+
+setwd("root")
 
 biolayers <- stack(list.files(pattern = '\\.tif$'))
 
 ocurs <- list.dirs(outfol, recursive = FALSE, full.names = FALSE)
 
-
-
-cropsC <- function(ocurs){
+cropsC <- function(species){
   
- 
-    narea <- paste0(outfol,ocurs, "/narea")
-    cat("doing", ocurs, "\n")
-    shapean <- readOGR(dsn = narea, layer = "narea")
-    #biolayers_cropc<-crop(biolayers, shapean) # variables predictoras cortadas al poligono de ocurrencias.
-    load(paste0(outfol,ocurs,"/narea/crop_narea.RDS"))
-    biolayers_cropc<-mask(biolayers_cropc, shapean) # variables predictoras cortadas al poligono de ocurrencias.
-
+  narea <- paste0(outfol,species, run_version ,"/bioclim")
+  
+  if(!file.exists(paste0(narea, "/", "narea.shp"))){
     
-    rm(shapean)
+    cat("Shapefile for", species,"native area doesn't exist", "\n")
+    
+  }else{
+    
+    cat("doing", species, "\n")
+    shapean <- readOGR(dsn = narea, layer = "narea")
+    biolayers_cropc<-crop(biolayers, shapean) # variables predictoras cortadas al poligono de ocurrencias.
+    biolayers_cropc<-mask(biolayers_cropc, shapean) # variables predictoras cortadas al poligono de ocurrencias.
+    
+    raster1<- biolayers_cropc[[1]]
+    raster1[Which(!is.na(raster1[]))] <- 1
+    
+    writeRaster(raster1, paste0(narea, "/", "narea_mask.tif"))
     save(biolayers_cropc, file = paste0(narea, "/", "crop_narea.RDS"))
     
-
-  rm(biolayers_cropc)
-  rm(narea)
+    
+    rm(narea)
+    rm(shapean)
+    rm(biolayers_cropc)
+    rm(raster1)
+  }
   
   gc(reset=TRUE)
   
@@ -39,3 +50,4 @@ c1 <- makeCluster(ncores)
 clusterEvalQ(c1, lapply(c("shapefiles", "raster", "rgeos", "rgdal"), library, character.only= TRUE))
 clusterExport(c1, c("outfol", "ocurs"))
 parLapply(c1, ocurs[1:200], cropsC)
+
