8523e9786c2ca93f2ce6d7e1329366ac75d9e07c
stevensotelo <stevenbetancurt@hotmail.com>
2018-02-02 10:04:18 -0500

    commented area_mask.R

--- src/tools/area_mask.R
+++ src/tools/area_mask.R
@@ -1,13 +1,13 @@
-################################################################
-##CALCULATE AREA FOR A MASK USING LATLONG RASTER APPROACH
-#################################################################
-
-require(raster)
-
-#loading mask
-mask<-raster("//dapadfs/Workspace_cluster_9/Aichi13/parameters/world_mask/raster/mask.tif")
-mask<-trim(mask)
-#calculating area
-am<-raster::area(mask,na.rm=T)
-#saving raster
-writeRaster(am,"//dapadfs/Workspace_cluster_9/Aichi13/parameters/world_mask/raster/area.tif")
+# ################################################################
+# ##CALCULATE AREA FOR A MASK USING LATLONG RASTER APPROACH
+# #################################################################
+# 
+# require(raster)
+# 
+# #loading mask
+# mask<-raster("//dapadfs/Workspace_cluster_9/Aichi13/parameters/world_mask/raster/mask.tif")
+# mask<-trim(mask)
+# #calculating area
+# am<-raster::area(mask,na.rm=T)
+# #saving raster
+# writeRaster(am,"//dapadfs/Workspace_cluster_9/Aichi13/parameters/world_mask/raster/area.tif")
