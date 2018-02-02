bf17c1fafc5d9dec696a17bfe167daf66823ff58
Julian Ramirez-Villegas <julian@MacBook-Air-3.local>
2018-02-02 13:42:41 -0500

    update nat_area_mask.R

--- src/1_modeling/nat_area_mask.R
+++ src/1_modeling/nat_area_mask.R
@@ -4,66 +4,60 @@
 # @param (string) species: species ID
 # @return (string): species ID
 nat_area_mask <- function(species) {
-  
-  #Loading global mask
-  global_mask<-raster(paste0(par_dir,"/","world_mask/raster/mask.tif")) ###ADD TO CONFIG
-  
-  #Loading Velox RDS climate object
-  rst_vx <- readRDS(paste(par_dir,"/biolayer_2.5/climate_vx.RDS",sep="")) ###ADD TO CONFIG
-  x<-rst_vx;rm(rst_vx)
-  #required packages
-  require(shapefiles); require(raster); library(rgeos);require(velox)
-  require(rgdal) #; require(parallel)
-  
   #load config function
   config(dirs=T, premodeling=T)
   
-  #native area folder
-  narea_dir <- paste0(gap_dir, "/",species, "/", run_version ,"/bioclim")
+  #required packages
+  require(shapefiles); require(raster); library(rgeos); require(velox)
+  require(rgdal) #; require(parallel)
   
-  #load native area shapefile
-  if (!file.exists(paste0(narea_dir, "/narea.shp"))) {
-    cat("Shapefile for species ID=", species,"native area doesn't exist", "\n")
-  } else {
-    #cat("doing", species, "\n")
-    if (!file.exists(paste0(narea_dir, "/", "crop_narea.RDS"))) {
-      #load native area shapefile
-      setwd(narea_dir)
-      shapean <- raster::shapefile(paste0(narea_dir,"/","narea.shp"))
-      
-      if (!file.exists(paste0(narea_dir, "/", "narea_mask.tif"))) {
+  #velox RDS climate object
+  x <- rst_vx
+  
+  #load counts
+  sp_counts <- read.csv(paste(gap_dir,"/",species,"/counts.csv",sep=""),sep="\t")
+  
+  #run only if there are records with coordinates
+  if (sp_counts$totalUseful != 0) {
+    #native area folder
+    narea_dir <- paste0(gap_dir, "/",species, "/", run_version ,"/bioclim")
+    
+    #load native area shapefile
+    if (!file.exists(paste0(narea_dir, "/narea.shp"))) {
+      cat("Shapefile for species ID=", species,"native area doesn't exist", "\n")
+    } else {
+      #cat("doing", species, "\n")
+      if (!file.exists(paste0(narea_dir, "/", "crop_narea.RDS"))) {
+        #load native area shapefile
+        shapean <- raster::shapefile(paste0(narea_dir,"/","narea.shp"))
+        shapean$value <- 1
         
-        #shapean <- raster:shapefile(dsn = "narea.shp", layer = "narea", verbose=F)
+        if (!file.exists(paste0(narea_dir, "/", "narea_mask.tif"))) {
+          na_msk <- raster::rasterize(shapean,global_mask,field="value",silent=T)
+          #na_msk[which(!is.na(na_msk[]))] <- 1
+          na_msk <- crop(na_msk, extent(shapean))
+          writeRaster(na_msk, paste0(narea_dir, "/", "narea_mask.tif"), format="GTiff")
+        } else {
+          na_msk <- raster(paste0(narea_dir, "/", "narea_mask.tif"))
+        }
         
-        na_msk<-raster::rasterize(shapean,global_mask,field=2,silent=T)
-        na_msk[which(!is.na(na_msk[]))]<-1
+        #crop and mask biolayers
+        x$crop(extent(shapean))
+        biolayers_cropc <- x$as.RasterStack()
+        biolayers_cropc <- biolayers_cropc * na_msk
+        names(biolayers_cropc) <- names(biolayers)
+        if (!biolayers_cropc@data@inmemory) {biolayers_cropc <- readAll(biolayers_cropc)}
         
-        writeRaster(na_msk, paste0(narea_dir, "/", "narea_mask.tif"), format="GTiff")
-      } else {
-        
-        na_msk<-raster(paste0(narea_dir, "/", "narea_mask.tif"))
-        
+        #save cropped biolayers dataset
+        saveRDS(object=biolayers_cropc, file=paste0(narea_dir, "/crop_narea.RDS"))
+        #rm(narea_dir); rm(shapean); rm(biolayers_cropc)
       }
-      
-      #crop and mask biolayers
-      
-      x$crop(extent(shapean))
-      x$write(path=paste0(narea_dir, "/", "crop_narea.tif"),overwrite = F);
-      biolayers_cropc<-stack(paste0(narea_dir, "/", "crop_narea.tif"))
-      biolayers_cropc<-biolayers_cropc*narea_mask
-      names(biolayers_cropc)<-names(biolayers)
-      
-      #save cropped biolayers dataset
-      
-      saveRDS(object=biolayers_cropc, file=paste0(narea_dir, "/crop_narea.RDS"))
-      file.remove(paste0(narea_dir, "/", "crop_narea.tif"),showWarnings=F)
-      
-      #rm(narea_dir); rm(shapean); rm(biolayers_cropc)
+      #clean memory
+      rm(x); gc(reset=TRUE)
     }
-    #clean memory and return species ID
-    gc(reset=TRUE)
-    return(species)
   }
+  #return species ID
+  return(species)
 }
 
 # testing
