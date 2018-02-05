2374d1ca3df1af7eccc89e8ee85c69e410dadcfc
Maria Victoria <diaz.maria@correounivalle.edu.co>
2018-02-05 15:26:20 -0500

    add function that counts records of the ocurrences for specie and creates a csv file

---
+++ src/tools/count_records_sp.R
@@ -0,0 +1,33 @@
+#María Victoria Díaz, Chrystian Sossa
+#CIAT, 2018
+#Function to know the species that has been modeled
+#@param (string) species_list: vector with list species IDs
+#@return (data frame): This function returns a csv file with the number of ocurrences by species and the ID.
+
+species_list<-list.files("//dapadfs/Workspace_cluster_9/Aichi13/parameters/occurrences/no_sea", pattern= ".csv$")
+
+species <- "2650133"
+
+uni<-function(species){
+data <- read.csv(paste("//dapadfs/Workspace_cluster_9/Aichi13/parameters/occurrences/no_sea/",species,".csv", sep=""),header=T)
+data <- data[which(!is.na(data$lon)),]
+data <- unique(data[,c("lon","lat")])
+rowsc <- nrow(data)
+base <- cbind(species, rowsc)
+return(base)
+
+}
+
+
+y <- lapply(1:length(species_list),function(i){
+   cat(i, "\n")
+   x<- uni(species_list[[i]])
+   return(x)
+   
+   
+ })
+
+x1<-write.csv(data, "//dapadfs/Workspace_cluster_9/Aichi13/runs/results/records.csv")
+
+
+
