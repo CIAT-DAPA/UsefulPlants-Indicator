d2d4cc2059dfc8a76dbfaf8cba0cbe2e52a1f7f1
Maria Victoria <diaz.maria@correounivalle.edu.co>
2018-02-06 07:49:29 -0500

    update function

--- src/tools/count_records_sp.R
+++ src/tools/count_records_sp.R
@@ -4,30 +4,31 @@
 #@param (string) species_list: vector with list species IDs
 #@return (data frame): This function returns a csv file with the number of ocurrences by species and the ID.
 
-species_list<-list.files("//dapadfs/Workspace_cluster_9/Aichi13/parameters/occurrences/no_sea", pattern= ".csv$")
+species_list<-list.files("//dapadfs/Workspace_cluster_9/Aichi13/parameters/occurrences/no_sea - Copy", pattern= ".csv$")
 
 species <- "2650133"
 
 uni<-function(species){
-data <- read.csv(paste("//dapadfs/Workspace_cluster_9/Aichi13/parameters/occurrences/no_sea/",species,".csv", sep=""),header=T)
-data <- data[which(!is.na(data$lon)),]
-data <- unique(data[,c("lon","lat")])
-rowsc <- nrow(data)
-base <- cbind(species, rowsc)
-return(base)
-
+  data <- read.csv(paste("//dapadfs/Workspace_cluster_9/Aichi13/parameters/occurrences/no_sea - Copy/",species, sep=""),header=T)
+  data <- data[which(!is.na(data$lon)),]
+  data <- unique(data[,c("lon","lat")])
+  rowsc <- nrow(data)
+  base <- cbind(species, rowsc)
+  return(base)
+  
 }
 
 
 y <- lapply(1:length(species_list),function(i){
-   cat(i, "\n")
-   x<- uni(species_list[[i]])
-   return(x)
-   
-   
- })
+  cat(i, "\n")
+  x<- uni(species_list[[i]])
+  return(x)
+  
+  
+})
 
 x1<-write.csv(data, "//dapadfs/Workspace_cluster_9/Aichi13/runs/results/records.csv")
+
 
 
 
