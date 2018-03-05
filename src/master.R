55bd1ee5435972a852815202d6ee86cd62294fe3
Maria Victoria <diaz.maria@correounivalle.edu.co>
2018-03-05 16:15:38 -0500

    Implementing the log file

--- src/master.R
+++ src/master.R
@@ -10,6 +10,7 @@ master_run <- function(species) {
   #error message
   message = "OK"
   status = TRUE
+  final_function= character()
   
   tryCatch({
     print(paste0("Start ",species))
@@ -21,21 +22,24 @@ master_run <- function(species) {
     #source(paste(repo_dir,"/tools/create_sp_dirs.R",sep=""))
     cat("...creating directories\n")
     spp_dirs <- create_sp_dirs(species)
-    
+    final_function = "0.create_sp_dirs was done"
     #step 2.1-clean sea
     #source(paste(repo_dir,"/0_cleaning/clean_sea.R",sep=""))
     cat("...cleaning species\n")
     spp_clean <- clean_sea(species)
+    final_function = "1.clean_sea was done"
     
     #step 2.2-create native area
     #source(paste(repo_dir,"/1_modeling/nat_area_shp.R",sep=""))
     cat("...creating native area shapefile\n")
     narea_shp <- nat_area_shp(species)
+    final_function = "2.nat_area_sh was done"
     
     #step 2.3-crop bioclim
     #source(paste(repo_dir,"/1_modeling/nat_area_mask.R",sep=""))
     cat("...masking bioclim layers to native area\n")
     crop_bio <- nat_area_mask(species)
+    final_function = "2.1.nat_area_mask was done"
     
     #step 3-modeling (#only calibration)
     #source(paste(repo_dir,"/1_modeling/1_1_maxent/modeling_approach.R",sep=""))
@@ -46,6 +50,8 @@ master_run <- function(species) {
     #source(paste(repo_dir,"/1_modeling/1_2_alternatives/create_buffers.R",sep=""))
     cat("...maxent modelling\n")
     spmod <- spModeling(species)
+    final_function = "3.spModeling was done"
+    
     
     #step 4.1-exsitu gap analysis
     #source(paste(repo_dir,"/2_gap_analysis/exsitu/srs.R",sep=""))
@@ -63,6 +69,7 @@ master_run <- function(species) {
     #source(paste(repo_dir,"/2_gap_analysis/exsitu/fcs.R",sep=""))
     cat("...exsitu fcs\n")
     fcs_ex <- fcs_exsitu(species)
+    final_function = "4. fcs_exsitu was calculated"
     
     #step 4.2-insitu gap analysis
     #source(paste(repo_dir,"/2_gap_analysis/insitu/grs.R",sep=""))
@@ -76,11 +83,15 @@ master_run <- function(species) {
     #source(paste(repo_dir,"/2_gap_analysis/insitu/fcs.R",sep=""))
     cat("...insitu fcs\n")
     fcs_in <- calculate_fcs(species)
+    final_function = "5. fcs_in was calculated"    
+    
     
     #step 4.3-combine insitu and exsitu
     #source(paste(repo_dir,"/2_gap_analysis/combined/fcs_combine.R",sep=""))
     cat("...combine exsitu and insitu fcs\n")
     fcs_comb <- fcs_combine(species)
+    final_function = "6. fcs combined was calculated"
+    
     
     #return status data.frame
     return (data.frame(species = species, status = status, message = message))
@@ -88,9 +99,10 @@ master_run <- function(species) {
     print(paste0("Error ",species))
     message = e
     status = FALSE
-    return(data.frame(species = species, status = status, message = message[[1]]))
   }, finally = {
     print(paste0("End ",species))
+    return(data.frame(species = species, status = status, message = message[[1]], final_function = final_function[[1]]))
+    
   })
 }
 
