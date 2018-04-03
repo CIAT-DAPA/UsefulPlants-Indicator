c6c8d9f00b84eb66bcab4fdbf8f6416c95310cf9
Maria Victoria <diaz.maria@correounivalle.edu.co>
2018-04-03 15:20:35 -0500

    Update indicators by uses and regions

---
+++ src/3_indicator/regions.js
@@ -0,0 +1,27 @@
+var data = [
+    ['Region Code', 'Sub-Continent', 'Index'],
+    ['021', 'Northern America', 0],
+    ['015','Northern Africa',0],
+    ['011','Western Africa',0],
+    ['017','Middle Africa',0],
+    ['014','Eastern Africa',0],
+    ['018','Southern Africa',0],
+    ['154','Northern Europe',0],
+    ['155','Western Europe',0],
+    ['151','Eastern Europe',0],
+    ['039','Southern Europe',0],
+    ['021','Northern America',0],
+    ['029','Caribbean',0],
+    ['013','Central America',0],
+    ['005','South America',0],
+    ['143','Central Asia',0.2967],
+    ['030','Eastern Asia',0],
+    ['034','Southern Asia',0],
+    ['035','South-Eastern Asia',0],
+    ['145','Western Asia',0.1433],
+    ['053','Australia and New Zealand',0],
+    ['054','Melanesia',0],
+    ['057','Micronesia',0],
+    ['061','Polynesia',0]
+
+];
--- src/3_indicator/uses.js
+++ src/3_indicator/uses.js
@@ -1,19 +1,17 @@
 
-
-var data = [
+var data = google.visualization.arrayToDataTable([
         ['Category', 'Understanding',{ role: 'style' }],
         ['Category', 1.0, 'opacity:0.5' ],
         ['Additive', 0, ''],
         ['Animal food', 0, ''],
         ['Bee plants', 0, ''],
-        ['Environmental', 0.4098, ''],
+        ['Environmental', 0.13132, ''],
         ['Fuels', 0, ''],
-        ['Genetic sources', 0.8097, ''],
+        ['Genetic sources', 0.100908, ''],
         ['Human food', 0, ''],
-        ['Materials',0, ''],
-        ['Medicine', 0, ''],
+        ['Materials',0.066269, ''],
+        ['Medicine', 0.048709, ''],
         ['Pesticide', 0, ''],
         ['Social', 0, '']
 		
-		 ];
-
+		 ]);
