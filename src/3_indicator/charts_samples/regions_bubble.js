8ed880c68742badf048b51781d913fff2996cbda
Daniel Amariles <daniel.amariles88@gmail.com>
2018-04-18 14:01:38 -0500

    adding new charts base code

---
+++ src/3_indicator/charts_samples/regions_bubble.js
@@ -0,0 +1,47 @@
+var data=[
+['Sub continents', 'HP', 'Richness', 'Continent'],
+[' Eastern Asia ',62.5766871165644,3826,'Asia'],
+[' Southern Asia ',62.0668693009119,3042,'Asia'],
+[' South America ',33.605812897366,2467, 'America'],
+[' Northern America ',56.2058526740666,2448, 'America'],
+[' South-Eastern Asia ',75.2665245202559,2357,'Asia'],
+[' Western Asia ',34.1416100872939,2274,'Asia'],
+[' Eastern Europe ',28.7549407114625,2258,'Europe'],
+[' Southern Europe ',15.5484558040469,2244,'Europe'],
+[' Central America ',37.5709421112372,2141, 'America'],
+[' Western Europe ',46.1538461538462,1771,'Europe'],
+[' Northern Africa ',32.7272727272727,1724,'Africa'],
+[' Eastern Africa ',51.7291066282421,1352,'Africa'],
+[' Australia and New Zealand ',46.1538461538462,1218, 'Oceania'],
+[' Southern Africa ',49.6385542168675,1210,'Africa'],
+[' Northern Europe ',6.56063618290259,1046,'Europe'],
+[' Central Asia ',35.2577319587629,1018,'Asia'],
+[' Middle Africa ',48.4320557491289,965,'Africa'],
+[' Caribbean ',25.7142857142857,806, 'America'],
+[' Western Africa ',52.0161290322581,783,'Africa'],
+[' Melanesia ',73.5955056179775,602, 'Oceania'],
+[' Micronesia ',82.6666666666667,133, 'Oceania'],
+[' Polynesia ',85.2459016393443,108, 'Oceania']
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
