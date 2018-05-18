9ae126f3f23b572329132444c9795da762728cbd
Daniel Amariles <daniel.amariles88@gmail.com>
2018-05-18 14:38:27 -0500

    adding external charts

---
+++ global.js
@@ -0,0 +1,13 @@
+var global=[
+['uses', 'Low Priority and Sufficiently Conserved', { role: 'style' }, 'Medium Priority',  { role: 'style' }, 'High Priority',  { role: 'style' }],
+['', 0 ,'', 0 ,'', 0 ,''],
+['Combined', 2.78, 'bar {'+'stroke-width: 10;' +'stroke-color: #12A356}', 53.75, 'bar {'+'stroke-width: 10;' +'stroke-color: #F0CB69}', 43.47, 'bar {'+'stroke-width: 10;' +'stroke-color: #E54D24}'],
+['', 0 ,'', 0 ,'', 0 ,''],
+['', 0 ,'', 0 ,'', 0 ,''],
+['ex situ', 3.28, '', 6.20, '', 90.52, ''],
+['', 0 ,'', 0 ,'', 0 ,''],
+['in situ', 40.67, '', 48.16, '', 11.17, ''],
+['', 0 ,'', 0 ,'', 0 ,''],
+['', 0 ,'', 0 ,'', 0 ,''],
+['', 0 ,'', 0 ,'', 0 ,'']
+];
