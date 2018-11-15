2da3b39a1e6529cb2778df7bc4ec19f08f3d4956
mariavictoriadiaz <35539148+mariavictoriadiaz@users.noreply.github.com>
2018-11-15 16:22:11 -0500

    Update ....
    to generate randomPoints according with the number of cells has the native area mask

--- src/1_modeling/1_1_maxent/nullModelAUC.R
+++ src/1_modeling/1_1_maxent/nullModelAUC.R
@@ -28,8 +28,33 @@ null_model_AUC<-function(species){
   Null_model <- geoDist(training, lonlat=T)
   
   # Step 4: Evaluate model
-  pabs <- randomPoints(mask=narea_mask, n=10000)
-  pabs <- as.data.frame(pabs)
+  count<-length(narea_mask[which(narea_mask[]==1)])
+  
+  
+  if(count>=10000){
+    pabs <- randomPoints(mask=narea_mask, n=10000)
+    pabs <- as.data.frame(pabs)
+    
+  }else{
+    
+    msk_narea <- narea_mask
+    msk_pts_narea <- raster::rasterToPoints(msk_narea)
+    msk_pts_narea <- as.data.frame(msk_pts_narea)
+    msk_pts_narea <- msk_pts_narea[which(msk_pts_narea$y > -60),]
+    msk_pts_narea$mask <- NULL
+    names(msk_pts_narea) <- c("lon", "lat")
+    msk_pts_narea <- msk_pts_narea[complete.cases(msk_pts_narea),]
+    msk_pts_narea$cellID <- cellFromXY(object = msk_narea, xy = msk_pts_narea[,c("lon", "lat")])
+    #occ_cellID <- cellFromXY(object = msk, xy = optPars@occ.pts[,c("LON","LAT")])
+    occ_cellID_narea <- cellFromXY(object = msk_narea, xy = occs[,c("lon","lat")])
+    #msk_pts_narea <- msk_pts_narea[setdiff(msk_pts_narea$cellID, occ_cellID),]; rm(occ_cellID)
+    msk_pts_narea <- msk_pts_narea[which(!msk_pts_narea$cellID %in% occ_cellID_narea),]
+    msk_pts_narea <- msk_pts_narea[,c("lon","lat")]
+    rownames(msk_pts_narea) <- 1:nrow(msk_pts_narea); rm(msk_narea)
+    pabs <- as.data.frame(msk_pts_narea)
+    
+  }
+  
   names(pabs) <- c("lon", "lat")
   eval <- dismo::evaluate(p=testing, a=pabs, model=Null_model)
   AUC <- eval@auc
