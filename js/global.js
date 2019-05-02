849d60a0e30a7b63e7e8b8b638f6da234d0559b3
Daniel Amariles <daniel.amariles88@gmail.com>
2019-05-02 16:28:27 -0500

    april 2019 data update

--- js/global.js
+++ js/global.js
@@ -1,10 +1,10 @@
 var global=[
 ['uses', 'Low Priority and Sufficiently Conserved', { role: 'style' }, 'Medium Priority',  { role: 'style' }, 'High Priority',  { role: 'style' }],
 ['', 0 ,'', 0 ,'', 0 ,''],
-['Combined', 2.78, 'bar {'+'stroke-width: 10;' +'stroke-color: #12A356}', 53.75, 'bar {'+'stroke-width: 10;' +'stroke-color: #F0CB69}', 43.47, 'bar {'+'stroke-width: 10;' +'stroke-color: #E54D24}'],
+['Combined', 2.8414827635944, 'bar {'+'stroke-width: 10;' +'stroke-color: #12A356}', 52.5890667820568, 'bar {'+'stroke-width: 10;' +'stroke-color: #F0CB69}', 44.5694504543488, 'bar {'+'stroke-width: 10;' +'stroke-color: #E54D24}'],
 ['', 0 ,'', 0 ,'', 0 ,''],
 ['', 0 ,'', 0 ,'', 0 ,''],
-['ex situ', 3.28, '', 6.20, '', 90.52, ''],
+['ex situ', 3.33189095629598, '', 6.1589499495168, '', 90.5091590941872, ''],
 ['', 0 ,'', 0 ,'', 0 ,''],
-['in situ', 40.67, '', 48.16, '', 11.17, ''],
+['in situ', 41.7568152315015, '', 45.1319774989182, '', 13.1112072695803, '']
 ];
